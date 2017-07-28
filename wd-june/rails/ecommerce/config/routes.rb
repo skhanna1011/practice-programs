005c73b5be54e818881ffe20bbe4cd478ae6c8ec
skhanna1011 <skhanna1011@gmail.com>
2017-07-28 17:54:37 +0530

    Updates backup

--- wd-june/rails/ecommerce/config/routes.rb
+++ wd-june/rails/ecommerce/config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get 'home/contact'
 
   resources :categories
+  resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
