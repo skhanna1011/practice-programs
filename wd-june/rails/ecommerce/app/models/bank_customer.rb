fe61adb3706ec1f0e9ab284e6f7b10530a0ed949
skhanna1011 <skhanna1011@gmail.com>
2017-08-07 17:56:12 +0530

    Updates Backup

--- wd-june/rails/ecommerce/app/models/bank_customer.rb
+++ wd-june/rails/ecommerce/app/models/bank_customer.rb
@@ -2,7 +2,7 @@ class BankCustomer < ApplicationRecord
   validates_presence_of :name, :acc_no, :balance
   validates_uniqueness_of :acc_no
   validates_numericality_of :balance, greater_than_or_equal_to: 0
-  
+
   def transact(amt,code)
     case code
     when 1
