c5bb57368b0c7c04c8a66e608fc1d23f93a0edd3
skhanna1011 <skhanna1011@gmail.com>
2017-08-30 15:59:15 +0530

    Adds working-api to backup

--- wd-june/rails/working-api/app/controllers/gender_controller.rb
+++ wd-june/rails/working-api/app/controllers/gender_controller.rb
@@ -1,7 +1,7 @@
 class GenderController < ApplicationController
   def search
     if params[:name]
-      response = HTTParty.get("https://gender-api.com/get?name=#{params[:name]}&key=pRPLCejozFwCHFbVCR")
+      response = HTTParty.get("https://gender-api.com/get?name=#{params[:name]}&key=api_key")
       @result = JSON.parse(response.body)
     end
   end
--- wd-june/rails/working-api/app/controllers/google_controller.rb
+++ wd-june/rails/working-api/app/controllers/google_controller.rb
@@ -2,7 +2,7 @@ class GoogleController < ApplicationController
   def distance
     # https://maps.googleapis.com/maps/api/distancematrix/json?origins=Vancouver+BC|Seattle&destinations=San+Francisco|Victoria+BC&mode=bicycling&language=fr-FR&key=api_key
     if params[:source] && params[:destination]
-      response = HTTParty.get("https://maps.googleapis.com/maps/api/distancematrix/json?origins=#{params[:source]}&destinations=#{params[:destination]}&mode=driving&language=en-EN&key=AIzaSyClh8vxsdRZtSIbrr81xCKu2Awr8BqbQAM")
+      response = HTTParty.get("https://maps.googleapis.com/maps/api/distancematrix/json?origins=#{params[:source]}&destinations=#{params[:destination]}&mode=driving&language=en-EN&key=api_key")
       @result = JSON.parse(response.body)
     end
   end
